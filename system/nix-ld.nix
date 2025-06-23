e589d7161c7f5957b6922b452c4e2c17fc7d2b01
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-06-23 10:08:02 -0700

    Add some more packages to nix-ld

--- system/nix-ld.nix
+++ system/nix-ld.nix
@@ -4,9 +4,13 @@
   programs.nix-ld = {
     enable = true;
     libraries = [
+      pkgs.sdl3
       pkgs.SDL2
       pkgs.SDL2_image
+      pkgs.SDL2_mixer
       pkgs.SDL2_ttf
+      pkgs.SDL2_gfx
+
       pkgs.xorg.libXinerama
       pkgs.libpng
       pkgs.freetype
