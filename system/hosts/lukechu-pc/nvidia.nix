66c8b973fc353e9b1017b93c9ecb8fa5548cc50a
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-06-30 16:47:09 -0700

    Add NVIDIA modesetting and powerManagement settings

--- system/hosts/lukechu-pc/nvidia.nix
+++ system/hosts/lukechu-pc/nvidia.nix
@@ -6,8 +6,11 @@
     "modesetting"
     "nvidia"
   ];
-  hardware.nvidia.open = false;
 
+  # GTX 1060 is not supported by the new open source NVIDIA kernel driver.
+  hardware.nvidia.open = false;
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
   hardware.nvidia.prime = {
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
