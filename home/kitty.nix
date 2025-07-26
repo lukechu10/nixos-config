f6c68bd7f0efa21ff349bc8fd385bbadb4c7acd2
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-07-25 17:59:36 -0700

    revert(kitty): actually it's a bit distracting

--- home/kitty.nix
+++ home/kitty.nix
@@ -8,8 +8,6 @@
       font_family = "FiraCode Nerd Font";
       confirm_os_window_close = 0;
       touch_scroll_multiplier = 3.0;
-      cursor_trail = 3;
-      cursor_trail_decay = "0.1 0.4";
     };
   };
 }
