8b044a0a1a8db1427934c577d2b5096d0606c7ad
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2026-02-21 19:20:39 +0000

    feat: add neovide

--- home/nvim.nix
+++ home/nvim.nix
@@ -37,4 +37,7 @@
   };
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/.config/nvim";
+
+  # Neovide
+  programs.neovide.enable = true;
 }
